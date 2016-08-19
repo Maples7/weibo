b03f61f710d783237ce9afaecf6e428268a9f82b
Maples7 <maples7@163.com>
2016-08-19 14:50:02 +0800

    微博列表草稿 | 需要进一步考虑复用和性能

--- controllers/weiboList.js
+++ controllers/weiboList.js
@@ -6,9 +6,19 @@ const status = require('../enums/resStatus');
 /**
  * 获取微博列表 - GET
  * @param {Object}      req
+ * @param {String}      [req.query.group]   - 某个分组（如：英语）成员的微博列表，默认查询全部
+ * @param {Number}      [req.query.limit]   - 默认为20
+ * @param {Number}      [req.query.offset]  - 默认为0
  * @param {Object}      res
  * @param {Function}    next
  */
 exports.getWeiboList = (req, res, next) => {
+    let options = {
+        group: req.query.group,
+        limit: req.query.limit,
+        offset: req.query.offset
+    };
 
+    return weibo.getWeiboList(/* ......... */)
+        .then(data => res.api(data)).catch(err => res.api_error(err.message));
 }
